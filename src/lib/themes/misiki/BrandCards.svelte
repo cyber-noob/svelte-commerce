ca0053d435354902775cc85281c36cc6db1aeb3d
Ajay <ajay0912.dpi@gmail.com>
2024-10-29 18:28:14 +0530

    /pets page redesign + logo

--- src/lib/themes/misiki/BrandCards.svelte
+++ src/lib/themes/misiki/BrandCards.svelte
@@ -2,53 +2,48 @@
   import OwnPuppyIcon from '$lib/icons/own_puppy.svg'
   import PuppyAccessory from '$lib/icons/puppy_accessories.svg'
   import PuppyHealth from '$lib/icons/puppy_health.svg'
+  import GoldenRetriever from '$lib/assets/goldPup-removebg-preview.png'
+  import { goto } from '$app/navigation'
 
   export let brand = {}
 
+  function createGradient(color: string) {
+    return `${color.from} ${color.via} ${color.to} bg-gradient-to-r`
+  }
 </script>
 
-<section>
-  <div class="flex w-3/4 gap-4 my-4">
+<section class="my-4">
+  <div class={`flex w-full gap-4 h-64 justify-evenly rounded-2xl ${createGradient(brand.bg_color)}`}>
     <img
       alt="logo"
-      src={brand?.image}
-      class="aspect-square w-96 h-80"
-    >
-
-    <div class="flex flex-col ml-4">
-      <div>
-        <div class="sm:text-lg text-zinc-900 font-bold border-l-4 border-green-400 p-4">
-          {brand?.title}
-        </div>
-        <div class="prose max-w-none text-sm text-zinc-500 mt-4">
-          {brand?.short_summary}
-        </div>
+      src={brand.image}
+      class="object-contain h-64 w-2/4"
+    />
+    <div class="flex flex-col w-full mr-8">
+      <span class="sm:text-3xl my-4 text-white font-extrabold border-l-4 border-white pl-4">
+        {brand.title}
+      </span>
+      <div class="my-4 prose max-w-none text-base font-bold text-zinc-800">
+        <span>
+          {brand.short_summary}
+        </span>
       </div>
-
-      <div class="mt-4">
-        <div class="flex m-4">
-          <img
-            alt="logo"
-            src={OwnPuppyIcon}
-            class="w-8 h-8"
-          />
-          <p class="text-sm text-green-400 m-2 underline underline-offset-8">Click here to get your own puppy ...</p>
-        </div>
-        <div class="flex m-4">
-          <img
-            alt="logo"
-            src={PuppyAccessory}
-            class="w-8 h-8"
-          />
-          <p class="text-sm text-green-400 m-2 underline underline-offset-8">Click here to get your special accessories for ...</p>
-        </div>
-        <div class="flex m-4">
-          <img
-            alt="logo"
-            src={PuppyHealth}
-            class="w-8 h-8"
-          />
-          <p class="text-sm text-green-400 m-2 underline underline-offset-8">Click here to get your pet checked ...</p>
+      <div class="flex flex-row justify-start">
+        <button class="m-4 p-2 bg-black text-sm text-white font-extrabold">
+          <span>
+            Explore Blogs
+          </span>
+        </button>
+        <button
+          on:click={() => goto(`/search?q=${encodeURIComponent(brand.q)}`)}
+          class="bg-black m-4 p-2 prose max-w-none text-sm text-white font-extrabold"
+        >
+         <span>
+           Explore Pets Collection
+         </span>
+        </button>
+        <div class="bg-black m-4 p-2 prose max-w-none text-sm text-white font-extrabold">
+          Explore Accessories
         </div>
       </div>
     </div>
